fcb28537b4d6980912c22a169fca975a40058279
TVD <thevalenciandev@gmail.com>
2020-09-12 18:51:03 +0100

    Adding bspwm and sxhkd. Moving common autostart apps into .xprofile and minor config clean up

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -2,12 +2,11 @@
 
 # Terminate already running bar instances
 killall -q polybar
-# If all your bars have ipc enabled, you can also use 
-# polybar-msg cmd quit
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 >>/tmp/polybar1.log 2>&1 & disown
-#polybar bar2 >>/tmp/polybar2.log 2>&1 & disown
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-echo "Bars launched..."
+# Launch "bar" (see config)
+polybar bar >>/tmp/polybar.log 2>&1 & disown
+
+echo "bar launched..."
